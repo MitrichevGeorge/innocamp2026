eec439cefcefebd5a0fcc915121c3897c9d7720a
George Mitrichev <Yegorim@mail.ru>
2026-07-06 16:11:40 +0300

    diet

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@
 
 # 1024 - 65535
 source env/bin/activate
-uvicorn src.a2:asgi_app --reload --host 127.0.0.1 --port 23008 --reload
+uvicorn src.app:asgi_app --reload --host 127.0.0.1 --port 23008 --reload
